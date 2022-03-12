3789caf8c0a27d26e2d8696589e3d6a1e1cef75e
PratimaGurav <prtmgrv2@gmail.com>
2022-03-12 20:42:30 +0000

    Add post view

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
-from . import views
+# from . import views
+from .views import (PostList, PostDetail, AddPostView)
 from django.urls import path
 
 urlpatterns = [
-    path("", views.PostList.as_view(), name="home"),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    path('add_post/', views.AddPostView.as_view(), name='add_post'),
+    path("", PostList.as_view(), name="home"),
+    path('<slug:slug>/', PostDetail.as_view(), name='post_detail'),
+    path('add_post/', AddPostView.as_view(), name='add_post'),
 ]
