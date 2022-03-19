7d15a6500cccf73960d5fb5cf079ec46cfe3a5b3
PratimaGurav <prtmgrv2@gmail.com>
2022-03-19 19:22:14 +0000

    Deployment commit

--- vegansjournal/settings.py
+++ vegansjournal/settings.py
@@ -30,9 +30,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['vegansjournal.herokuapp.com', 'localhost']
-
 X_FRAME_OPTIONS = 'SAMEORIGIN'
+
+ALLOWED_HOSTS = ['vegansjournal.herokuapp.com', 'localhost']
 
 # Application definition
 
