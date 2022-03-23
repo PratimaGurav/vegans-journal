f068f312efeb911f30b012b63e6e994ab4145fa4
PratimaGurav <prtmgrv2@gmail.com>
2022-03-23 14:10:24 +0000

    Update formatting for urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 # from . import views
 from django.urls import path
-from .views import (PostList, PostDetail, AddPostView, like_view, EditPostView, DeletePostView, AddCategoryView, CategoryView, CategoryListView, results_view)
+from .views import (PostList, PostDetail, AddPostView, like_view, EditPostView,
+                    DeletePostView, AddCategoryView, CategoryView,
+                    CategoryListView, results_view)
 
 
 urlpatterns = [
@@ -9,8 +11,10 @@ urlpatterns = [
     path('add_post/', AddPostView.as_view(), name='add_post'),
     path('add_category/', AddCategoryView.as_view(), name='add_category'),
     path('like/<str:slug>', like_view, name='post_like'),
-    path('post-detial/edit/<str:slug>/', EditPostView.as_view(), name='edit_post'),
-    path('post-detial/<str:slug>/delete', DeletePostView.as_view(), name='delete_post'),
+    path('post-detial/edit/<str:slug>/', EditPostView.as_view(),
+         name='edit_post'),
+    path('post-detial/<str:slug>/delete', DeletePostView.as_view(),
+         name='delete_post'),
     path('category/<str:cats>/', CategoryView, name='category'),
     path('category_list/', CategoryListView, name='category_list'),
     path('search_blog', results_view, name='search_blog'),
