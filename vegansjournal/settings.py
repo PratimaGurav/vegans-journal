4c42c338d8d7002c48801a82e55d0c25608a4073
PratimaGurav <prtmgrv2@gmail.com>
2022-03-24 21:27:13 +0000

    Update settings.py file

--- vegansjournal/settings.py
+++ vegansjournal/settings.py
@@ -142,7 +142,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
